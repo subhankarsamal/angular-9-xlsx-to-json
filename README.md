ed77547a03727e014fcb7e8cf77d81f228ae3d48
subhankarsamal <samal.subhankar.1991@gmail.com>
2020-04-13 20:19:26 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# xlsx-to-json-kgfkrb
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/xlsx-to-json-kgfkrb)
---
+++ src/app/app.component.ts
@@ -0,0 +1,48 @@
+import { Component } from '@angular/core';
+import * as XLSX from 'xlsx';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  name = 'This is XLSX TO JSON CONVERTER';
+  willDownload = false;
+
+  constructor() { }
+
+  onFileChange(ev) {
+    let workBook = null;
+    let jsonData = null;
+    const reader = new FileReader();
+    const file = ev.target.files[0];
+    reader.onload = (event) => {
+      const data = reader.result;
+      workBook = XLSX.read(data, { type: 'binary' });
+      jsonData = workBook.SheetNames.reduce((initial, name) => {
+        const sheet = workBook.Sheets[name];
+        initial[name] = XLSX.utils.sheet_to_json(sheet);
+        return initial;
+      }, {});
+      const dataString = JSON.stringify(jsonData);
+      document.getElementById('output').innerHTML = dataString.slice(0, 300).concat("...");
+      this.setDownload(dataString);
+    }
+    reader.readAsBinaryString(file);
+  }
+
+
+  setDownload(data) {
+    this.willDownload = true;
+    setTimeout(() => {
+      const el = document.querySelector("#download");
+      el.setAttribute("href", `data:text/json;charset=utf-8,${encodeURIComponent(data)}`);
+      el.setAttribute("download", 'xlsxtojson.json');
+    }, 1000)
+  }
+
+
+
+
+}
